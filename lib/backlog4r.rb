ce5755715fae09bdbad4b0fea5a6d3a175f639c7
Keisuke Futonaka <futonaka@gmail.com>
2013-10-26 22:32:41 +0900

    Added api accessor by method missing.

--- lib/backlog4r.rb
+++ lib/backlog4r.rb
@@ -1,26 +1,32 @@
 require "backlog4r/version"
 require "uri"
 require "xmlrpc/client"
+require "active_support/core_ext"
 
 module Backlog4r
-  class Backlog
-    def initialize(space_name, user_name, password)
+  class Backlog < XMLRPC::Client::Proxy
+    def initialize(space_name, user_id, password)
       space_uri  = URI.parse("https://#{space_name}.backlog.jp/XML-RPC")
       proxy_host = proxy_port = nil
       use_ssl    = true
       timeout    = 5
 
-      client = XMLRPC::Client.new(space_uri.host,
+      server = XMLRPC::Client.new(space_uri.host,
                                   space_uri.path,
                                   space_uri.port,
                                   proxy_host,
                                   proxy_port,
-                                  user_name,
+                                  user_id,
                                   password,
                                   use_ssl,
                                   timeout,
                                  )
-      client.proxy "backlog"
+      super(server, "backlog")
+    end
+
+    def method_missing(action, *args)
+      action = action.to_s.camelize(:lower).to_sym
+      super
     end
   end
 end
